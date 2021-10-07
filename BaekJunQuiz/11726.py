d768581ee93dbbe8adf8fbbe4c1149e25eff329d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-10-07 20:42:27 +0900

    BaekJun Dynamic Programming 4problems

---
+++ BaekJunQuiz/11726.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+memo = [0]*(N+2)
+memo[1] = 1
+memo[2] = 2
+def solution(N):
+    if N == 1:
+        return memo[1]
+    elif N == 2:
+        return memo[2]
+    for i in range(3, N + 2):
+        memo[i] = memo[i-2] + memo[i-1]
+    return memo[N]
+        
+
+
+
+print(solution(N) % 10007)
---
+++ BaekJunQuiz/9095.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+def solution(N):
+    if N == 1:
+        return memo[0]
+    elif N == 2:
+        return memo[1]
+    elif N == 3:
+        return memo[2]
+    for i in range(4, N + 1):
+        memo[i-1] = memo[i-2] + memo[i-3] + memo[i-4]
+    return memo[N-1]
+
+for _ in range(T):
+    N = int(input())
+    memo = [0]*(N+2)
+    memo[0] = 1
+    memo[1] = 2
+    memo[2] = 4
+    print(solution(N))
