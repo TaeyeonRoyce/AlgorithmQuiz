905e10d5aa1818ccac350a3211d06ebf11088fe1
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-11-11 23:37:57 +0900

    BaekJun 2021/11/11

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,12 +1,12 @@
 package solution;
 
-import solution.boj.level1.Boj_1316;
-import solution.programmers.level1.prog_004;
+
+import solution.greedy.Boj_11047;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        Boj_1316 solution = new Boj_1316();
-        solution.solution();
+        new Boj_11047().solution();
     }
 }
+
