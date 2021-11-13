99cbde1a20754e45d776b4a6a36908369a6dbe56
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-11-14 00:23:41 +0900

    BaekJun 2021/11/13

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,12 +1,20 @@
 package solution;
 
 
-import solution.greedy.Boj_11047;
+import solution.bfs.Boj_2667;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        new Boj_11047().solution();
+        new Boj_2667().solution();
     }
 }
+
+
+
 
