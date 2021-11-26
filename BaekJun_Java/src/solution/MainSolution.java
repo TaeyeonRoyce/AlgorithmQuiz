8b40f8d169f60aa00dd5397a1919b0c42e6b07dc
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-11-27 01:19:30 +0900

    BaekJun 2021/11/26

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,11 +1,12 @@
 package solution;
 
+import solution.greedy.Boj_1931;
 import solution.recursion.Boj_6603;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        new Boj_6603().solution();
+        new Boj_1931().solution();
     }
 }
 
---
+++ BaekJun_Java/src/solution/binarysearch/Boj_1654.java
@@ -0,0 +1,40 @@
+package solution.binarysearch;
+
+import java.util.Scanner;
+
+public class Boj_1654 {
+    public void solution() {
+        Scanner inputs = new Scanner(System.in);
+        int K = inputs.nextInt();
+        int N = inputs.nextInt();
+        int[] lines = new int[K];
+        long maxLine = 0;
+
+        for (int i = 0; i < K; i++) {
+            lines[i] = inputs.nextInt();
+            if (lines[i] > maxLine) {
+                maxLine = lines[i];
+            }
+        }
+
+        maxLine += 1;
+        long min = 0;
+        long mid = 0;
+
+        while (min < maxLine) {
+            int cnt = 0;
+            mid = (maxLine + min) / 2;
+
+            for (int line : lines) {
+                cnt += line / mid;
+            }
+
+            if (cnt < N) {
+                maxLine = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        System.out.println(min - 1);
+    }
+}
