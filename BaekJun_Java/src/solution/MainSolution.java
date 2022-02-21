41fcffcbd09bf869365102ba2e619ba7aac8e96d
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-02-21 23:51:13 +0900

    BaekJun 2022/02/21

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -2,12 +2,13 @@ package solution;
 
 import socarTest.BracketSolution;
 import solution.binarysearch.Boj_10816;
+import solution.implemention.Boj_15829;
 import solution.math.Boj_11050;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        new BracketSolution().solution("()(){{}");
+        new Boj_15829().solution();
     }
 }
 
