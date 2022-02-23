702b8ee54516c2573f3a38f520857fc1b4f94d3e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-02-23 23:27:42 +0900

    BaekJun 2022/02/23

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,14 +1,11 @@
 package solution;
 
-import socarTest.BracketSolution;
-import solution.binarysearch.Boj_10816;
-import solution.implemention.Boj_15829;
-import solution.math.Boj_11050;
+import solution.binarysearch.Boj_2805;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        new Boj_15829().solution();
+        new Boj_2805().solution();
     }
 }
 
