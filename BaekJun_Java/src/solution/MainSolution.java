4eb65a57858b5f44a64d62b8ad20b737f776c340
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-02-25 22:33:42 +0900

    BaekJun 2022/02/25

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,12 +1,11 @@
 package solution;
 
-import solution.binarysearch.Boj_2805;
-import solution.implemention.Boj_18111;
+import solution.recursion.Boj_1074;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        new Boj_18111().solution();
+        new Boj_1074().solution();
     }
 }
 
