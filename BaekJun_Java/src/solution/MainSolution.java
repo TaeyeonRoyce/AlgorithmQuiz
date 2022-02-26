d599382cdf986e2a848fe563ebd95b082bdc48f7
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-02-27 00:29:05 +0900

    BaekJun 2022/02/27

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,11 +1,12 @@
 package solution;
 
 import solution.datastructure.Boj_1764;
+import solution.datastructure.Boj_1927;
 
 public class MainSolution {
 
     public static void main(String[] args) {
-        new Boj_1764().solution();
+        new Boj_1927().solution();
     }
 }
 
