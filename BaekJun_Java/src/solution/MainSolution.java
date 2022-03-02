a20f92961e882bf44ec9e3ce25c7eb9d95343490
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-03-02 10:19:42 +0900

    BaekJun 2022/03/01

--- BaekJun_Java/src/solution/MainSolution.java
+++ BaekJun_Java/src/solution/MainSolution.java
@@ -1,12 +1,15 @@
 package solution;
 
+import java.io.IOException;
+
+import solution.datastructure.Boj_11279;
 import solution.datastructure.Boj_1764;
 import solution.datastructure.Boj_1927;
 
 public class MainSolution {
 
-    public static void main(String[] args) {
-        new Boj_1927().solution();
+    public static void main(String[] args) throws IOException {
+        new Boj_11279().solution();
     }
 }
 
