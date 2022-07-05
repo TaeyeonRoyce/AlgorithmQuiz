7d1a7648bc674764f895dc433d187423d9c26b12
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-07-05 13:38:43 +0900

    Boj_25307

--- withJava/src/Boj_Solution/MainSolution.java
+++ withJava/src/Boj_Solution/MainSolution.java
@@ -2,18 +2,13 @@ package Boj_Solution;
 
 import java.io.IOException;
 
-import Boj_Solution.bruteforce.Boj_2992;
-import Boj_Solution.datastructure.Boj_10876;
-import Boj_Solution.datastructure.Boj_5052;
-import Boj_Solution.greedy.Boj_1946;
-import Boj_Solution.implemention.Boj_1010;
+import Boj_Solution.bfs.Boj_25307;
 import Boj_Solution.programmers.level1.Prog_005;
 
 public class MainSolution {
 
     public static void main(String[] args) throws IOException {
-        int[] solution = new Prog_005().solution(new String[] {"muzi", "frodo", "apeach", "neo"},
-            new String[] {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"}, 2);
+        new Boj_25307().solution();
     }
 
 }
